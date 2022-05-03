639d2af1be9f76cbd788405174b5b7e362d87f00
mendukrishna <90266074+mendukrishna@users.noreply.github.com>
2022-05-03 22:32:15 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,20 @@
+<?php       
+        
+$con = mysqli_connect("localhost","root","","veggies");
+if (isset($_REQUEST['username'])) {
+        // removes backslashes
+        $username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+        $username = mysqli_real_escape_string($con, $username);
+        $email    = stripslashes($_REQUEST['email']);
+        $email    = mysqli_real_escape_string($con, $email);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        $query    = "INSERT into `register` (username, password, email)
+                     VALUES ('$username', '" . md5($password) . "', '$email')";
+       $result   = mysqli_query($con, $query);
+    $err="<font color='blue'>Registration successfull !!</font>";
+    
+    } 
+?>
+    
